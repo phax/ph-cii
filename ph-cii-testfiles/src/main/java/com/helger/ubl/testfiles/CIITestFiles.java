2da96bac35a57bf28f80498967f5e1a3d9520834
Philip Helger <philip@helger.com>
2016-10-18 16:03:00 +0200

    Added initial test files

---
+++ ph-cii-testfiles/src/main/java/com/helger/ubl/testfiles/CIITestFiles.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.ubl.testfiles;
+
+import java.util.List;
+
+import javax.annotation.concurrent.Immutable;
+
+import com.helger.commons.annotation.CodingStyleguideUnaware;
+import com.helger.commons.collection.CollectionHelper;
+
+/**
+ * List of all CII test files categorized by version.
+ *
+ * @author Philip Helger
+ */
+@Immutable
+@CodingStyleguideUnaware
+public final class CIITestFiles
+{
+  public static final List <String> D16A1_FILES = CollectionHelper.makeUnmodifiable ("test-cii/d16a1/CII_example1.xml",
+                                                                                     "test-cii/d16a1/CII_example2.xml",
+                                                                                     "test-cii/d16a1/CII_example3.xml",
+                                                                                     "test-cii/d16a1/CII_example4.xml",
+                                                                                     "test-cii/d16a1/CII_example5.xml",
+                                                                                     "test-cii/d16a1/CII_example6.xml",
+                                                                                     "test-cii/d16a1/CII_example7.xml",
+                                                                                     "test-cii/d16a1/CII_example8.xml",
+                                                                                     "test-cii/d16a1/CII_example9.xml");
+
+  public CIITestFiles ()
+  {}
+}
