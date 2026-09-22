ace0da88953e5badbcb947a13cf4c0f9ad68d3a9
Philip Helger <philip@helger.com>
2026-09-22 13:57:35 +0200

    Added tests

---
+++ ph-cii-d22b/src/test/java/com/helger/cii/d22b/CCIID22BTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2024-2026 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.cii.d22b;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.validation.Schema;
+
+import org.junit.Test;
+
+import com.helger.collection.commons.CommonsArrayList;
+import com.helger.collection.commons.ICommonsList;
+import com.helger.io.resource.ClassPathResource;
+import com.helger.xml.schema.XMLSchemaCache;
+
+/**
+ * Test class for class {@link CCIID22B}.
+ *
+ * @author Philip Helger
+ */
+public final class CCIID22BTest
+{
+  @Test
+  public void testGetXSDResource ()
+  {
+    final ICommonsList <ClassPathResource> aXSDs = new CommonsArrayList <> (CCIID22B.getXSDResourceCII (),
+                                                                            CCIID22B.getXSDResourceCDAR ());
+    for (final ClassPathResource aXSD : aXSDs)
+    {
+      assertTrue (aXSD.getPath (), aXSD.exists ());
+
+      final Schema aSchema = XMLSchemaCache.getInstanceOfClassLoader (aXSD.getClassLoader ()).getSchema (aXSD);
+      assertNotNull (aXSD.getPath (), aSchema);
+    }
+  }
+}
---
+++ ph-cii-d25a/src/test/java/com/helger/cii/d25a/CCIID25ATest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2026 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.cii.d25a;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import javax.xml.validation.Schema;
+
+import org.junit.Test;
+
+import com.helger.collection.commons.CommonsArrayList;
+import com.helger.collection.commons.ICommonsList;
+import com.helger.io.resource.ClassPathResource;
+import com.helger.xml.schema.XMLSchemaCache;
+
+/**
+ * Test class for class {@link CCIID25A}.
+ *
+ * @author Philip Helger
+ */
+public final class CCIID25ATest
+{
+  @Test
+  public void testGetXSDResource ()
+  {
+    final ICommonsList <ClassPathResource> aXSDs = new CommonsArrayList <> (CCIID25A.getXSDResourceCII ());
+    for (final ClassPathResource aXSD : aXSDs)
+    {
+      assertTrue (aXSD.getPath (), aXSD.exists ());
+
+      final Schema aSchema = XMLSchemaCache.getInstanceOfClassLoader (aXSD.getClassLoader ()).getSchema (aXSD);
+      assertNotNull (aXSD.getPath (), aSchema);
+    }
+  }
+}
